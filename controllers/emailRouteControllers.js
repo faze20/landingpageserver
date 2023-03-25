e75c3dfa308b8538148f0fd8a7bfc069d5f3ffee
faze20 <42577269+faze20@users.noreply.github.com>
2023-03-25 13:31:30 -0700

    modeldb

--- controllers/emailRouteControllers.js
+++ controllers/emailRouteControllers.js
@@ -1,29 +1,29 @@
 import asyncHandler from 'express-async-handler';
-import { SMTPClient } from 'emailjs';
+// import { SMTPClient } from 'emailjs';
 
 
 export const sendEmail = asyncHandler(async (req, res) => {
     console.log(req.query.body , req.headers);
 
-    const client = new SMTPClient({
-        user: 'olanrewajuadekola59@gmail.com',
-        password: 'Iyaibeji59',
-        host: 'smtp.gmail.com',
-        ssl: true,
-      });
-        // send the message and get a callback with an error or details of the message that was sent
-        const sendProspectingNotofication = client.send(
-          {
-            text: 'test emaill',
-            from: 'olanrewajuadekola59@gmail.com',
-            to: 'afeez20@gmail.com',
-            cc: 'afeez20@hotmail.com',
-            subject: 'testing emailjs',
-          },
-          (err, message) => {
-            console.log(err || message);
-          }
-        );
+    // const client = new SMTPClient({
+    //     user: 'softwaredevbytes@gmail.com',
+    //     password: 'Iyaibeji9952',
+    //     host: 'smtp.gmail.com',
+    //     ssl: true,
+    //   });
+    //     // send the message and get a callback with an error or details of the message that was sent
+    //     const sendProspectingNotofication = client.send(
+    //       {
+    //         text: 'test emaill',
+    //         from: 'olanrewajuadekola59@gmail.com',
+    //         to: 'afeez20@gmail.com',
+    //         cc: 'afeez20@hotmail.com',
+    //         subject: 'testing emailjs',
+    //       },
+    //       (err, message) => {
+    //         console.log(err || message);
+    //       }
+    //     );
         // console.log(sendProspectingNotofication)
       res.status(200).send({message:'email sent'});
    
