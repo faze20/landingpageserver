034e557e53241d3ec2b3ac3d0535a3982c4f4f0d
faze20 <42577269+faze20@users.noreply.github.com>
2023-04-01 11:45:58 -0700

    corsissue1

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ var corsOptions = {
    
 // app.options("*", cors())
 app.use(function(req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", allowedWebsite);
+    res.setHeader("Access-Control-Allow-Origin", corsOptions(allowedWebsite));
     res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     next();
